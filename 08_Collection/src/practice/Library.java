6a4b259c9efcb00ef97782201f2d890cf453ec01
ehdgud1919 <rlaehdgud55@naver.com>
2023-01-25 14:47:25 +0900

    2023-01-25

--- 08_Collection/src/practice/Library.java
+++ 08_Collection/src/practice/Library.java
@@ -25,50 +25,86 @@ public class Library {
 		String author = sc.next();
 		Book book = new Book(isbn, title, author);
 		books.add(book);
-		System.out.println(title + "책이 추가되었습니다.");
+		System.out.println(title + " 책이 추가되었습니다.");
 	}
 	
-	private void deleteBook() {
+	private void deleteBook() {	// RuntimeException은 UnChecked-Exception이므로 throws RuntimeException을 생략할 수 있다.
 		System.out.println("=== 책 삭제하기 ===");
-		try {	// 둘 다 사용 가능 isEmpty < 공백 검사 메소드
+			// 둘 다 사용 가능 isEmpty < 공백 검사 메소드
 			if(books.size() == 0 || books.isEmpty()) {
 				throw new RuntimeException("등록된 책이 없습니다.");
 			}
 			System.out.println("삭제할 isbn >>>");
 			String isbn = sc.next();
+			
 			if(isbn.isEmpty() == false) {
-			for(int i = 0; i < books.size(); i++) {
-				Book book = books.get(i);
-				if(isbn.equals(book.getIsbn())) {
-					Book deletedBook = books.remove(i);		// remove(i)는 삭제된 요소(Book)를 반환한다. "Book deletedBook =" < 생략 가능
-					System.out.println(deletedBook + "책이 삭제되었습니다.");
-					return;
+				
+				//	1. 객체 기반 삭제(Book 객체 간 동등 비교가 필요하기 때문에 equals 메소드를 오버라이드(@Override) 해야 한다.
+				for(Book book : books) {
+					if(isbn.equals(book.getIsbn())) {
+						books.remove(book);		// equals 메소드가 내부에서 사용된다.
+						System.out.println(book + " 책이 삭제되었습니다.");
+						return;
+					}
 				}
-			}
+			
+				/* 2. 인덱스 기반 삭제	
+				for(int i = 0; i < books.size(); i++) {		// 향상for문을 안 쓰고 일반 for문을 사용하는 이유는 인덱스 정보 때문이다.
+					Book book = books.get(i);				// 삭제할 때 인덱스가 필요하기 때문
+					if(isbn.equals(book.getIsbn())) {
+						Book deletedBook = books.remove(i);		// remove(i)는 삭제된 요소(Book)를 반환한다. "Book deletedBook =" < 생략 가능
+						System.out.println(deletedBook + "책이 삭제되었습니다.");
+						return;
+					}
+				}*/
 		}
-		throw new RuntimeException(isbn + "을 가진 책이 없습니다.");
-	}catch(Exception e) {
-		System.out.println(e.getMessage());
-	}
-	
+		throw new RuntimeException(isbn + " isbn을 가진 책이 없습니다.");
 	
 	
 }
 	
-	private void findBook() {
+	private void findBook() throws RuntimeException{	// RuntimeException은 UnChecked-Exception이므로 throws RuntimeException을 생략할 수 있다.
+		
+		System.out.println("=== 책 조회하기 ===");
+		
+			if(books.isEmpty()) {
+				throw new RuntimeException("등록된 책이 없습니다.");
+			}
+			System.out.println("조회할 isbn >>>");
+			String isbn = sc.next();
+			if(!isbn.isEmpty()) {
+				for(Book book : books) {
+					if(isbn.equals(book.getIsbn())) {
+						System.out.println("조회 결과 :" + book);
+						return;
+					}
+				}
+			}
+			throw new RuntimeException(isbn + " isbn을 가진 책이 없습니다.");
+
 		
 	}
 	
-	private void printAllBooks() {
+	private void printAllBooks() throws RuntimeException{	// RuntimeException은 UnChecked-Exception이므로 throws RuntimeException을 생략할 수 있다.
+		
+		System.out.println("=== 전체 조회하기 ===");
+		
+			if(books.isEmpty()) {
+				throw new RuntimeException("등록된 책이 없습니다.");
+			}
+			for(Book book : books) {
+				System.out.println(book);
+			}
+
 		
 	}
 	
 	public void manage() {
 		
 		while(true) {
+			try {
 			System.out.println("1.추가 2.삭제 3.조회 4.전체 0.종료 >>>");
 			String choice = sc.next();
-			
 			switch(choice) {
 			case "1":
 				addBook();
@@ -87,6 +123,9 @@ public class Library {
 				return;
 			default:
 				System.out.println("잘못된 입력입니다. 다시 시도하세요.");
+			}
+				}catch(Exception e) {
+					System.out.println(e.getMessage());
 			}
 		}
 		
