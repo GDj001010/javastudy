5607859371b7f04284dbb8ee5509c0076ba37472
ehdgud1919 <rlaehdgud55@naver.com>
2023-01-26 17:25:29 +0900

    2023-01-26

--- 09_IO/src/ex02_Writer/JSONMainClass.java
+++ 09_IO/src/ex02_Writer/JSONMainClass.java
@@ -1,5 +1,14 @@
 package ex02_Writer;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
@@ -74,7 +83,7 @@ public class JSONMainClass {
 		
 	}
 	
-	public static void ex03() {
+	public static void ex03() {   // 공부하기
 		/*
 		{
 			"model": "세탁기",
@@ -102,7 +111,7 @@ public class JSONMainClass {
 		
 	}
 	
-	public static void ex04() {
+	public static void ex04() {    		// 공부하기
 		
 		// [{"model":"세탁기","maker":"삼성","price":100},{"model":"냉장고","maker":"LG","price":200}]
 		// String 형식의 JSON 데이터 (배열)  []
@@ -133,9 +142,62 @@ public class JSONMainClass {
 		
 	}
 	
+	public static void ex05() {
+		
+		Map<String, Object> product1 = new HashMap<String, Object>();
+		
+		product1.put("model", "세탁기");
+		product1.put("maker", "삼성");
+		product1.put("price", 100);
+		
+		Map<String, Object> product2 = new HashMap<String, Object>();
+		
+		product2.put("model", "냉장고");
+		product2.put("maker", "LG");
+		product2.put("price", 200);
+		
+		Map<String, Object> product3 = new HashMap<String, Object>();
+		
+		product3.put("model", "TV");
+		product3.put("maker", "삼성");
+		product3.put("price", 300);
+		
+		List<Map<String, Object>> products = Arrays.asList(product1,product2, product3);
+		
+		// List, Set 부모 → Collection
+		JSONArray arr = new JSONArray(products);
+		String content = arr.toString();
+		
+		File dir = new File("C:" + File.separator + "storage");
+		if(dir.exists() == false) {
+			dir.mkdirs();
+		}
+		File file = new File(dir, "product.json");
+		
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(file))){
+			bw.write(content);
+			System.out.println("product.json 파일이 생성됨.");
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		
+		/*
+		try(BufferedWriter bw = new BufferedWriter(new FileWriter(file))){
+			for(Object ob : arr) {
+				JSONObject obj = (JSONObject)ob;
+				bw.write(obj.getString("model"));
+				bw.write(obj.getString("maker"));
+				bw.write(obj.getInt("price"));
+				System.out.println("product.json 파일이 생성됨.");
+			}
+		}
+		*/
+	}
+	
+	
 	public static void main(String[] args) {
 		
-		ex04();
+		ex05();
 		
 	}
 
