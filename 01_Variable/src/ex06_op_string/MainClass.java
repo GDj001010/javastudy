7cf9fe81a738475b498bf0f79033a32bb5f99938
ehdgud1919 <rlaehdgud55@naver.com>
2023-01-26 21:29:34 +0900

    2023-01-26

--- 01_Variable/src/ex06_op_string/MainClass.java
+++ 01_Variable/src/ex06_op_string/MainClass.java
@@ -51,7 +51,7 @@ public class MainClass {
 	
 	
 	public static void main(String[] args) {
-		ex03();
+		ex01();
 
 	}
 
--- 06_Practice/src/example01/MainClass.java
+++ 06_Practice/src/example01/MainClass.java
@@ -4,10 +4,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		
-		String name = "김동형";
-		
-		String age = "30";
-		
+
 	 
 		
 
