c13809384502d662d3788e5803a8d3ef0bf99833
ehdgud1919 <rlaehdgud55@naver.com>
2023-01-30 10:48:51 +0900

    2023-01-30

--- 09_IO/src/ex04_OutputStream/MainClass.java
+++ 09_IO/src/ex04_OutputStream/MainClass.java
@@ -1,9 +1,14 @@
 package ex04_OutputStream;
 
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainClass {
 
@@ -82,10 +87,145 @@ public class MainClass {
 		
 	}
 	
+	public static void ex03() {
+		
+		File dir = new File("C:" + File.separator + "storage");
+		File file = new File(dir, "ex03.bin");
+		
+		BufferedOutputStream bos = null;
+		
+		try {
+			
+			bos = new BufferedOutputStream(new FileOutputStream(file));
+			
+			bos.write("반갑습니다\n또만나요".getBytes("UTF-8"));
+			
+			
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(bos != null) {
+					bos.close();
+				}
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("ex03.bin 파일의 크기 : " + file.length() + "바이트");
+		
+	}
+	
+	public static void ex04() {
+		
+		// 변수를 그대로 출력하는 DataOutputStream   	 String 값을 많이 주고 받음
+		
+		File dir = new File("C:" + File.separator + "storage");
+		File file = new File(dir, "ex04.dat");
+		
+		DataOutputStream dos = null;
+		
+		try {
+			dos = new DataOutputStream(new FileOutputStream(file));
+			
+			// 출력할 변수
+			String name = "에밀리";					// 9   바이트
+			int age = 30;							// 4
+			double height = 180.5;					// 8
+			boolean isAlive = true;
+			
+			// 출력 (변수 타입에 따라서 메소드가 다르다.)
+			dos.writeUTF(name);
+			dos.writeInt(age);
+			dos.writeDouble(height);
+			dos.writeBoolean(isAlive);
+			
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(dos != null) {
+					dos.close();
+				}
+			}catch (IOException e) {
+				e.printStackTrace();
+		}
+		}
+		
+		System.out.println("ex04.dat 파일의 크기 : " + file.length() + "바이트");
+	}
+	
+	public static void ex05() {
+		
+		// 객체를 그대로 출력하는 ObjectOutputStram
+		
+		File dir = new File("C:" + File.separator + "storage");
+		File file = new File(dir, "ex05.dat");
+		
+		ObjectOutputStream oos = null;
+		
+		try {
+			
+			oos = new ObjectOutputStream(new FileOutputStream(file));
+			
+			// 출력할 객체
+			List<Person> people = Arrays.asList(
+				new Person("에밀리", 30, 180.5, true),
+				new Person("제시카", 35, 190.5, true)
+			);
+			
+			// 출력할 객체
+			Person person = new Person();
+			person.setName("제임스");
+			person.setAge(40);
+			person.setHeight(170.5);
+			person.setAlive(false);
+			
+		}catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(oos != null) {
+					oos.close();
+				}
+			}catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("ex05.dat 파일의 크기 : " + file.length() + "바이트");
+		
+	}
+	
 	public static void main(String[] args) {
 		
-		ex02();
+		ex05();
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
