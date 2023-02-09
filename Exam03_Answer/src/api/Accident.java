f914747d84a222df11b6bc833b0cc367881eebf2
ehdgud1919 <rlaehdgud55@naver.com>
2023-02-09 17:42:41 +0900

    2023-02-06

--- Exam03_Answer/src/api/Accident.java
+++ Exam03_Answer/src/api/Accident.java
@@ -2,56 +2,39 @@ package api;
 
 public class Accident {
 	
-	private String occrrncDt;  // 발생월일시 
-
-	private String occrrncDayCd;  // 발생요일코드 
-
-	private int dthDnvCnt;  // 사망자수 
+	private String occrrncDt;
+	private String occrrncDayCd;
+	private int dthDnvCnt;
+	private int injpsnCnt;
 	
-	private int injpsnCnt;  // 부상자수 
-
-	
-	
-	
-	
-	
-	
-
 	public String getOccrrncDt() {
 		return occrrncDt;
 	}
-
 	public void setOccrrncDt(String occrrncDt) {
 		this.occrrncDt = occrrncDt;
 	}
-
 	public String getOccrrncDayCd() {
 		return occrrncDayCd;
 	}
-
 	public void setOccrrncDayCd(String occrrncDayCd) {
 		this.occrrncDayCd = occrrncDayCd;
 	}
-
 	public int getDthDnvCnt() {
 		return dthDnvCnt;
 	}
-
 	public void setDthDnvCnt(int dthDnvCnt) {
 		this.dthDnvCnt = dthDnvCnt;
 	}
-
 	public int getInjpsnCnt() {
 		return injpsnCnt;
 	}
-
 	public void setInjpsnCnt(int injpsnCnt) {
 		this.injpsnCnt = injpsnCnt;
 	}
-
-	
-	
-	
-	
+	@Override
+	public String toString() {
+		return "Accident [occrrncDt=" + occrrncDt + ", occrrncDayCd=" + occrrncDayCd + ", dthDnvCnt=" + dthDnvCnt
+				+ ", injpsnCnt=" + injpsnCnt + "]";
+	}
 	
 }
